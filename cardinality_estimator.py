f4610664097e81223c28a2cc13cc5493690df899
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2025-12-25 20:53:20 +0100

    abstract class

--- cardinality_estimator.py
+++ cardinality_estimator.py
@@ -1,3 +1,33 @@
-class CardinaltyEstimator:
-    def __init__(self):
+from abc import ABC, abstractmethod
+import random
+import randomhash
+
+class CardinalityEstimator(ABC):
+    """
+    Abstract base class for cardinality estimators.
+    """
+
+    def __init__(self, seed=None, num_hashes=10):
+        self.seed = seed
+        random.seed(self.seed)
+        rfh = randomhash.RandomHashFamily(count=num_hashes)
+        self.reset()
+
+    @abstractmethod
+    def reset(self):
+        """Reset internal state."""
+        pass
+
+    @abstractmethod
+    def add(self, element):
+        """Process one element from the stream."""
+        pass
+
+    @abstractmethod
+    def estimate(self) -> float:
+        """Return the cardinality estimate."""
+        pass
+
+    def hs(self):
+        """Compute the family of hashes"""
         pass
